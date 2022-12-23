253330ed97b51af01b7c3cf2cb662cdd73c264fa
Oskar Hermansson <oskar.hermansson@tradera.com>
2022-12-23 10:58:39 +0100

    Day 1

--- Day1.cs
+++ Day1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace advent_of_code_2022;
 
@@ -6,11 +8,41 @@ public class Day1 : IDay
 {
     public long PartOne(string[] input)
     {
-        throw new NotImplementedException();
+        return GetElves(input).Max();
     }
 
     public long PartTwo(string[] input)
     {
-        throw new NotImplementedException();
+        return GetElves(input).OrderByDescending(e => e).Take(3).Sum();
+    }
+
+    private static List<int> GetElves(string[] input)
+    {
+        var elves = new List<int>();
+        var currentElf = 0;
+        foreach (var line in input)
+        {
+            if (line == string.Empty)
+            {
+                elves.Add(currentElf);
+                currentElf = 0;
+            }
+            else
+            {
+                currentElf += int.Parse(line);
+            }
+        }
+
+        if (currentElf > 0)
+        {
+            elves.Add(currentElf);
+        }
+
+        foreach (var elf in elves)
+        {
+            Console.WriteLine(elf);
+        }
+
+        return elves;
     }
 }
