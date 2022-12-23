8cddb44142a470472ccc58829903e628818ecfc7
Oskar Hermansson <oskar.hermansson@tradera.com>
2022-12-23 10:52:11 +0100

    Refactor to regular methods

--- Program.cs
+++ Program.cs
@@ -12,11 +12,7 @@ internal class Program
 
     public static void Main(string[] args)
     {
-        if (args.Length > 0)
-        {
-            RunDay(args);
-        }
-        else
+        if (args.Length < 1)
         {
             foreach (var dayType in DayTypes)
             {
@@ -24,41 +20,40 @@ internal class Program
             }
 
             Console.Write("Day: ");
-            RunDay(Console.ReadLine().Split(" "));
+            args = Console.ReadLine()?.Split(" ") ?? Array.Empty<string>();
         }
 
-        static string[] GetInput(int dayNumber, bool useExample)
+        if (int.TryParse(args[0], out var dayNumber))
         {
-            var suffix = useExample ? "example" : "input";
-            var fileName = $"Day{dayNumber}.{suffix}";
-            return File.Exists(fileName) ? File.ReadAllLines(fileName) : Array.Empty<string>();
-        }
-
-        static IDay? GetDay(int dayNumber)
-        {
-            var dayType = DayTypes.FirstOrDefault(t => t.Name == "Day" + dayNumber);
-            return dayType != null ? (IDay?)Activator.CreateInstance(dayType) : null;
-        }
-
-        static void RunDay(string[] args)
-        {
-            if (int.TryParse(args[0], out var dayNumber))
+            var day = GetDay(dayNumber);
+            if (day == null)
             {
-                var day = GetDay(dayNumber);
-                if (day == null)
-                {
-                    Console.WriteLine($"Day {dayNumber} is not implemented");
-                    return;
-                }
-
-                var dayInput = GetInput(dayNumber, args.Length > 1 && args[1] == "example");
-                Console.WriteLine($"Day {dayNumber} part 1: {day.PartOne(dayInput)}");
-                Console.WriteLine($"Day {dayNumber} part 2: {day.PartTwo(dayInput)}");
+                Console.WriteLine($"Day {dayNumber} is not implemented");
             }
             else
             {
-                Console.WriteLine("Could not parse argument to day number");
+                var useExample = args.Length > 1 && args[1] == "example";
+                var input = GetInput(dayNumber, useExample);
+                Console.WriteLine($"Day {dayNumber} part 1: {day.PartOne(input)}");
+                Console.WriteLine($"Day {dayNumber} part 2: {day.PartTwo(input)}");
             }
         }
+        else
+        {
+            Console.WriteLine("Could not parse argument to day number");
+        }
+    }
+
+    private static IDay? GetDay(int dayNumber)
+    {
+        var dayType = DayTypes.FirstOrDefault(t => t.Name == "Day" + dayNumber);
+        return dayType != null ? (IDay?)Activator.CreateInstance(dayType) : null;
+    }
+
+    private static string[] GetInput(int dayNumber, bool useExample)
+    {
+        var suffix = useExample ? "example" : "input";
+        var fileName = $"Day{dayNumber}.{suffix}";
+        return File.Exists(fileName) ? File.ReadAllLines(fileName) : Array.Empty<string>();
     }
 }
