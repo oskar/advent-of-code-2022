76ccd5a248c8401bbe99d00a0d20d26e92595fd4
Oskar Hermansson <oskar.hermansson@tradera.com>
2022-12-24 15:21:48 +0100

    Day 4

---
+++ Day4.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace advent_of_code_2022;
+
+public class Day4 : IDay
+{
+    private static readonly Regex Pattern = new Regex("(\\d+)-(\\d+),(\\d+)-(\\d+)");
+
+    public long PartOne(string[] input)
+    {
+        return input.Select(Parse).Count(l =>
+            (l.FirstLower <= l.SecondLower && l.FirstUpper >= l.SecondUpper) ||
+            (l.FirstLower >= l.SecondLower && l.FirstUpper <= l.SecondUpper));
+    }
+
+    public long PartTwo(string[] input)
+    {
+        return input.Select(Parse).Count(l => l.FirstUpper >= l.SecondLower && l.SecondUpper >= l.FirstLower);
+    }
+
+    private static (int FirstLower, int FirstUpper, int SecondLower, int SecondUpper) Parse(string line)
+    {
+        var match = Pattern.Match(line);
+        return (int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value),
+            int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value));
+    }
+}
